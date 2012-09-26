84d8a8368421bf9e1927eefe4a0f17816e9ed277
lop11106@uvg.edu.gt <lop11106@uvg.edu.gt@fefd5acd-23b1-ef12-ae02-a1c6765a4e8b>
2012-09-26 04:22:23 +0000

    Procedimiento CONCA con problemas, se congela al ejecutarse.

--- trunk/LAB7.asm
+++ trunk/LAB7.asm
@@ -18,26 +18,34 @@ TITLE LAB7
 .DATA
 
 OPCION    	DB   ?
-MSJMENU 	DB   ' Que desea hacer:   								', 0DH, 0AH
-			DB	 ' 1. Ingreso de articulo   						', 0DH, 0AH 
-			DB	 ' 2. Despliegue de articulo   						', 0DH, 0AH 
-			DB	 ' 3. Despliegue de inventario total de artículos 	', 0DH, 0AH
-			DB	 ' 4. Borrar articulo   							', 0DH, 0AH
-			DB	 ' 5. Salida   										', 0DH, 0AH, '$'
-MSJMENU1	DB  'Ingrese el numero de la opcion que desea realizar: ','$'
-M_ING   	DB  0DH,0AH,'Ingrese el numero de codigo: $'
-M_INGIN 	DB  0DH,0AH,'Ha realizado un ingreso invalido. Repita su ingreso.$'			
-PRIMD		DB  ? 
+MSJMENU 	DB   ' Que desea hacer:   								 ', 0DH, 0AH
+			DB	 ' 1. Ingreso de articulo   						 ', 0DH, 0AH 
+			DB	 ' 2. Despliegue de articulo   						 ', 0DH, 0AH 
+			DB	 ' 3. Despliegue de inventario total de artículos 	 ', 0DH, 0AH
+			DB	 ' 4. Borrar articulo   							 ', 0DH, 0AH
+			DB	 ' 5. Salida   										 ', 0DH, 0AH, '$'
+MSJMENU1	DB   'Ingrese el numero de la opcion que desea realizar: ','$'
+MSJCADENA   DB   'Ingrese una cadena de no mas de 12 caracteres:', 0DH, 0AH, '$'
+M_ING   	DB   0DH,0AH,'Ingrese el numero de codigo: $'
+M_INGIN 	DB   0DH,0AH,'Ha realizado un ingreso invalido. Repita su ingreso.$'			 
 MSJ1     	DB   'Eligio sumar$'
 MSJ2		DB 	 'Eligio restar$'
 MSJ3 		DB   'Eligio multiplicar$' 
 TABLA   	DW   PRO1              ; Tabla de bifurcación con sus tres opciones
 			DW   PRO2
 			DW   PRO3
+			DW	 PRO4
+			DW	 PRO5
+LISTA  	    LABEL BYTE                    				 ; inicio de la lista de parametros
+MAXLEN 	    DB   13                       				 ; numero maximo de caracteres de entrada
+ACTLEN 	    DB   0                        				 ; numero real de caracteres de entrada
+DESCRIP	    DB   12 DUP (' '), 0DH, 0AH, '$'             ; caracteres introducidos del teclado
+LINEA	    DB   14 DUP (' '), 0DH, 0AH, '$'
+ENTR1       DB   0DH,0AH,'$'
 
-ENTR1        DB   0DH,0AH,'$'
-SECD    DB  ?
-RES     DW  ?
+SECD    	DB   ?
+PRIMD		DB   ?
+RES     	DW   ?
 ;-------------------------------------------------------------------------------------------
 ; Inicio de código
 .CODE
@@ -105,7 +113,7 @@ REP_ING:  LEA   DX, M_ING                   ;Imprime la petición de ingreso al u
           JB    INVALIDO                    ;De estarlo, se repite la petición.
           CMP   AL, 9                       ;Se verifica que el ingreSo no esté arriba del valor superior.
           JA    INVALIDO
-          CALL  MULTI
+          CALL  CONCA
           RET                               ;Si se llega aquí, el ingreso el válido.
 INVALIDO: LEA   DX, M_INGIN                 ;De ser invalido el ingreso, se imprime un mensaje informándolo.
           CALL  MOSTRAR
@@ -113,24 +121,29 @@ INVALIDO: LEA   DX, M_INGIN                 ;De ser invalido el ingreso, se impr
           JMP   REP_ING                     ;Se repite el ingreso.
 GET_ING   ENDP
 
-;MULTI: Se encarga de realizar las multiplicaciones necesarias para guardar el numero de dos 
-;       digitos en el arreglo "Notas".
+;CONCA: Se encarga de concatener los dígitos ingresados para guardarlos como Strings en la cadena correspondiente. 
 
-MULTI   PROC  NEAR
-        MOV   SECD, AL
-        MOV   AL, 10
-		MUL   PRIMD
-		MOV   RES, AX
-		XOR   AX, AX
-		MOV   AL, SECD
-		ADD   RES, AX
+CONCA   PROC  NEAR
+		ADD   SECD, 30H
+		ADD   PRIMD, 30H
+		MOV   CX, 1
+		LEA   SI, SECD
+		LEA   DI, LINEA
+COP:	MOVSB
+		JMP   COP
+		INC   DI
+		LEA   SI, PRIMD
+		LEA   DI, LINEA
+COPI:	MOVSB
+		JMP   COPI
+		LEA   DX, LINEA
+		CALL  MOSTRAR
 		RET
-MULTI    ENDP
+CONCA   ENDP
 
 ;Procedimiento de la tabla
 PRO1 	PROC NEAR
-		LEA DX, MSJ1
-		CALL MOSTRAR
+		CALL GET_ING
         RET
 PRO1 	ENDP
 
@@ -147,6 +160,14 @@ PRO3 	PROC NEAR
 		CALL MOSTRAR
         RET
 PRO3 	ENDP
+
+PRO4	PROC NEAR
+		RET
+PRO4	ENDP
+
+PRO5	PROC NEAR
+		RET
+PRO5	ENDP
 
 ;Tomado del ejemplo "Tablas.asm" de Martha Ligia Naranjo
 SALTOS	PROC 	NEAR
